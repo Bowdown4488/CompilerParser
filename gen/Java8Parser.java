6f30805720482e87276368ac81a05f53937a45ff
KillerTrex1 <37259376+KillerTrex1@users.noreply.github.com>
2019-11-03 02:39:51 +0800

    parse walking

--- gen/Java8Parser.java
+++ gen/Java8Parser.java
@@ -1,4 +1,4 @@
-// Generated from C:/Users/Paolo/IdeaProjects/ParserTest\Java8Parser.g4 by ANTLR 4.7.2
+// Generated from C:/Users/Paolo/IdeaProjects/CompilerParser\Java8Parser.g4 by ANTLR 4.7.2
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.*;
--- gen/Java8ParserBaseListener.java
+++ gen/Java8ParserBaseListener.java
@@ -1,4 +1,4 @@
-// Generated from C:/Users/Paolo/IdeaProjects/ParserTest\Java8Parser.g4 by ANTLR 4.7.2
+// Generated from C:/Users/Paolo/IdeaProjects/CompilerParser\Java8Parser.g4 by ANTLR 4.7.2
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
@@ -10,6 +10,8 @@ import org.antlr.v4.runtime.tree.TerminalNode;
  * of the available methods.
  */
 public class Java8ParserBaseListener implements Java8ParserListener {
+
+
 	/**
 	 * {@inheritDoc}
 	 *
